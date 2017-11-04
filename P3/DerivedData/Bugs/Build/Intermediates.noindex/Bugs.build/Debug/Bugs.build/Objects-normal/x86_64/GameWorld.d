4b712f2a1d3d2af415c36616edb18e1ed02a0ae5
quentintruong <quentintruong@users.noreply.github.com>
2017-11-04 15:25:50 -0700

    pathing

---
+++ P3/DerivedData/Bugs/Build/Intermediates.noindex/Bugs.build/Debug/Bugs.build/Objects-normal/x86_64/GameWorld.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameWorld.cpp \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameWorld.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameConstants.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameController.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/SpriteManager.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut_std.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut_ext.h
---
+++ P3/DerivedData/Bugs/Build/Intermediates.noindex/Bugs.build/Debug/Bugs.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,7 @@
+dependencies: /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/main.cpp \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameController.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/SpriteManager.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut_std.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/freeglut_ext.h \
+  /Users/quentintruong/Desktop/UCLA-CS32-W17/P3/Bugs/GameConstants.h
